1e2ce6faae8f6aa6200505acddef4034d1fc2bd7
Omar Hany <ben_hany@Omars-MacBook-Pro.local>
2025-09-09 01:23:24 +0300

    Created a function that returns all the data about a team

--- data.py
+++ data.py
@@ -4,8 +4,14 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+token = os.getenv("API_TOKEN")
+
+if not token:
+    raise RuntimeError("Missing API_Token in environment or .env file")
+
 uri = 'https://api.football-data.org/v4/matches?season=2024&limit=5'
-headers = {'X-Auth-Token' : os.getenv("api_token")} 
+headers = {'X-Auth-Token' : os.getenv("API_TOKEN")} 
 
 response = requests.get(uri, headers=headers)
 print(f"Status code is: {response.status_code}")
@@ -13,8 +19,20 @@ print(f"Response keys are: {list(response.json().keys())}")
 print(f"Number of matches: {len(response.json()['matches'])}")
 top_six_teams = [57, 66, 61, 65, 64, 73]
 
-for match in response.json()['matches']:
-    print(match) 
 
-if len(response.json()['matches']) == 0:
-    print("No matches were found")
+def get_matches(team_id):
+    uri = f"https://api.football-data.org/v4/teams/{team_id}/matches?competitions=PL&season=2024"
+    response = requests.get(uri, headers=headers)
+    data = response.json()
+    
+    result_set = data.get("resultSet", {})
+    matches = data.get("matches", [])
+
+    print(f"Team ID: {team_id}")
+    print(f"Total number of matches {result_set.get('count', 0)}")
+    print(f"Wins: {result_set.get('wins', 0)}")
+    print(f"Draws: {result_set.get('draws', 0)}")
+    print(f"Losses: {result_set.get('losses', 0)}")
+    print(f"Matches returned: {len(matches)}")
+
+get_matches(64)
